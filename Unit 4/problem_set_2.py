2bc149f2801ede9bbd88d861920e3638281636ba
Kyrylo <kiril201313bigmir.net@gmail.com>
2024-10-10 00:02:42 -0400

    Unit 4, S2, PS1, #1

--- Unit 4/problem_set_2.py
+++ Unit 4/problem_set_2.py
@@ -1,7 +1,8 @@
 # Problem 1: Merge Sorted Lists (Problem set 2 unintentionally, but still useful :) )
 
 # U - Understand:
-# Can the lists be empty?
+# Can one of the list the list be empty?
+# What if the lists are different sizes
 # Needs to implement same direction pointer of two-pointer approach
 
 # P - Plan:
@@ -51,3 +52,58 @@ def merge_sorted_lists(lst1, lst2):
 # lst2 = [2, 4, 6]
 # merged_lst = merge_sorted_lists(lst1, lst2)
 # print(merged_lst)
+
+# Problem 1: Long Pressed
+
+# U-Understand:
+# Needs to implement same direction pointer of two-pointer approach
+
+
+# P-Plan:
+# In English:
+# Initialize name pointer and set it to 0
+# Initialize typed pointer and set it to 0
+# While typed pointer is less than the length of typed:
+#   If name pointer is less than the length of name and current character of name is the same as the curent character of typed string
+#       Increment the name pointer
+#       Increment the typed pointer
+#   Else if the typed pointer is greater than 0 and the value of the current pointer is equal to the value at the previous position of the pointer
+#       Increment the type pointer
+#   Else
+#       return False (it is not a long-press)
+# return the chheck name pointer is equal to the length of name
+
+
+# I-Implement
+def is_long_pressed(name, typed):
+    name_pointer = 0
+    typed_pointer = 0
+
+    while typed_pointer < len(typed):
+        if name_pointer < len(name) and name[name_pointer] == typed[typed_pointer]:
+            # If the characters match, move both pointers
+            name_pointer += 1
+            typed_pointer += 1
+        elif typed_pointer > 0 and typed[typed_pointer] == typed[typed_pointer - 1]:
+            # If the current character in 'typed' matches the previous character,
+            # it means it was long-pressed, so only move 'typed_pointer'
+            typed_pointer += 1
+        else:
+            # If there's no match and it's not a long-press, return False
+            return False
+
+    # After processing all characters in 'typed', ensure that all characters in 'name' were matched
+    return name_pointer == len(name)
+
+
+name = "alex"
+typed = "aaleex"
+print(is_long_pressed(name, typed))
+
+name2 = "saeed"
+typed2 = "ssaaedd"
+print(is_long_pressed(name2, typed2))
+
+name3 = "courtney"
+typed3 = "courtney"
+print(is_long_pressed(name3, typed3))
